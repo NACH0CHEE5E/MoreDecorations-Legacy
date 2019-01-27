6bad97743f824ef522c831d9cc59dcdb0e4bbf5c
NACH0CHEE5E <39394001+NACH0CHEE5E@users.noreply.github.com>
2019-01-27 22:38:56 +1000

    i dont know

--- Types/GenerateTypes/slabs/standard.cs
+++ Types/GenerateTypes/slabs/standard.cs
@@ -2,6 +2,18 @@
 using static ItemTypesServer;
 using Pipliz.JSON;
 using MoreDecorations.Types.GenerateTypes.Config;
+using Newtonsoft.Json;
+using AI;
+using Jobs;
+using Newtonsoft.Json;
+using NPC;
+using Pipliz;
+using Pipliz.JSON;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Random = System.Random;
 
 namespace Nach0.Decor.GenerateTypes.Slab
 {
@@ -15,7 +27,15 @@ namespace Nach0.Decor.GenerateTypes.Slab
         {
             var i = 0;
             public static JSONNode JsonSerialize<T>(this T obj)
-            //public static string[] typeList = JSON.Deserialize(GenerateTypeConfig.modGamedataPath);
+            {
+                var objStr = JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+                return JSON.DeserializeString(objStr);
+            }
+
+            public static T JsonDeerialize<T>(this JSONNode node)
+            {
+                return JsonConvert.DeserializeObject<T>(node.ToString(), new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+            }
         }
 
         [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup, GenerateTypeConfig.generateRecipesPrefix + type)]
