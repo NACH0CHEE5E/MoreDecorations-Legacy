f88f778128edaec885195653e374e9fa68a0456e
Jim Burlison <JBurlison@Gmail.com>
2019-01-27 07:40:06 -0500

    Removed directories that should not be checked in, added gitignore so they dont get re-added Added objects to make a CS items

---
+++ Models/Colliders.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Pipliz.JSON;
+using UnityEngine;
+
+namespace MoreDecorations.Models
+{
+    public class Colliders
+    {
+        public class Boxes
+        {
+            public Vector3 min { get; private set; }
+            public Vector3 max { get; private set; }
+
+            public Boxes() { }
+
+            public Boxes(Vector3 minCollide, Vector3 maxCollide)
+            {
+                min = minCollide;
+                max = maxCollide;
+            }
+        }
+
+        public bool collidePlayer { get; private set; }
+        public bool collideSelection { get; private set; }
+        public List<Boxes> boxes { get; private set; } = new List<Boxes>();
+
+        public Colliders() { }
+
+        public Colliders (bool colideplayer, bool collideselection, List<Boxes> collider)
+        {
+            collidePlayer = colideplayer;
+            collideSelection = collideselection;
+            boxes = collider;
+        }
+    }
+}
